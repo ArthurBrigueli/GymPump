852fc0c03792e52ee1bec78117eb9530b92b2d5d
Arthur Brigueli <arthurbriguelifort@gmail.com>
2024-03-08 21:42:30 -0300

    feat: indicador do treino do dia
    Agora o usuario tera em sua tela um indicador de qual treino eh o treino do dia
    
    exemplo: o usuario criou um treino no dia 08/03/2024 e ele colocou a data do treino 08/03/2024, entao sera adicionado um indicador que mostrara que eh o treino do dia

--- GymPump/components/TreinoItem.jsx/TreinoList.jsx
+++ GymPump/components/TreinoItem.jsx/TreinoList.jsx
@@ -1,13 +1,39 @@
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
 import { noteStyle } from '../../styles/Note/noteStyle'
+import { format } from 'date-fns'
 
 const TreinoList = ({name, data})=>{
+
+    const date = new Date()
+    const dataform = format(date, 'dd/MM/yyyy')
+
     return(
-        <View>
-            <Text style={noteStyle.txtNome}>{name}</Text>
-            <Text>{data}</Text>
+        <View style={styles.container}>
+            <View>
+                <Text style={noteStyle.txtNome}>{name}</Text>
+                <Text>{data}</Text>
+            </View>
+            {dataform == data && (
+                <View style={styles.containerDate}>
+                </View>
+            )}
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    containerDate: {
+        width: 15,
+        height: 15,
+        backgroundColor: '#271A45',
+        borderRadius: 100
+    }
+})
 
 export default TreinoList
